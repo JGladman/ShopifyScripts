d6c8b02f04995a5cfb2585510447a66854a123fc
jacobg <jacobgladman32@gmail.com>
2022-02-19 12:13:18 -0500

    Added static product translations/home/jacobg/anaconda3/bin/python

--- scripts/translation_script.py
+++ scripts/translation_script.py
@@ -25,6 +25,32 @@ def translate(row):
             row["Translated content"] = value["Product Name (FR)"].iloc[0]
     if row["Default content"] == "Brand Description":
         row["Translated content"] = "Description de la Marque"
+    elif (
+        row["Default content"]
+        == "Brand Name: {{ product.metafields.my_fields.brand_name.value }}"
+    ):
+        row[
+            "Translated content"
+        ] = "Marque: {{ product.metafields.my_fields.brand_name.value }}"
+    elif (
+        row["Default content"] == "Size: {{ product.metafields.my_fields.size.value }}"
+    ):
+        row[
+            "Translated content"
+        ] = "Taille: {{ product.metafields.my_fields.size.value }}"
+    elif row["Default content"] == "Product Description":
+        row["Translated content"] = "Description du Produit"
+    elif row["Default content"] == "Ingredients":
+        row["Translated content"] = "Ingrédients"
+    elif row["Default content"] == "Product Information":
+        row["Translated content"] = "Information Produit"
+    elif (
+        row["Default content"]
+        == "<p>Country of Origin: {{ product.metafields.my_fields.country_of_origin.value }}</p><p></p><p>Product ID Number 1: {{ product.metafields.my_fields.product_id_1.value }}</p><p></p><p>Product ID Number 2: {{ product.metafields.my_fields.product_id_2.value }}</p><p></p><p>(Can dynamically render DIN/NPN if present on product. Missing for most)</p>"
+    ):
+        row[
+            "Translated content"
+        ] = "<p>Pays d'origine: {{ product.metafields.my_fields.country_of_origin.value }}</p><p></p><p>Numéro d'identification du produit 1: {{ product.metafields.my_fields.product_id_1.value }}</p><p></p><p>Numéro d'identification du produit 2: {{ product.metafields.my_fields.product_id_2.value }}</p><p></p><p>(Can dynamically render DIN/NPN if present on product. Missing for most)</p>"
     # Translate Metafields
     return row
 
