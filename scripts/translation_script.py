281ecb8b6f4774a911779de871dba16bfe5fb768
jacobg <jacobgladman32@gmail.com>
2022-02-21 11:09:03 -0500

    Included description translation

--- scripts/translation_script.py
+++ scripts/translation_script.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sqlalchemy import true
 
 items = pd.read_csv(
     r"/home/jacobg/Documents/Code/ShopifyScripts/spreadsheets/The_Natural_Store_translations_Feb-17-2022.csv"
@@ -7,8 +8,31 @@ items = pd.read_csv(
 items.to_excel("unformatted.xlsx")
 
 translated_values = pd.read_excel(
-    r"/home/jacobg/Documents/Code/ShopifyScripts/spreadsheets/Website document .xlsx"
+    r"/home/jacobg/Documents/Code/ShopifyScripts/spreadsheets/WebsiteDocument2.xlsx",
 )
+
+
+def feature_mapping(desc):
+    if type(desc) == str:
+        return desc.replace("\r", "").replace("\n", "").replace("_x000D_", "")
+    else:
+        return desc
+
+
+translated_values["Key Product Features"] = translated_values[
+    "Key Product Features"
+].map(feature_mapping)
+
+count = 0
+
+
+def set_count():
+    global count
+    count += 1
+
+
+def print_count():
+    print(count)
 
 
 def translate(row):
@@ -16,7 +40,6 @@ def translate(row):
     # if row["Type"] == "COLLECTION":
     #     print("COLLECTION")
 
-    # Translate product names
     if row["Type"] == "PRODUCT" and row["Field"] == "title":
         value = translated_values.loc[
             translated_values["Product Name"] == row["Default content"]
@@ -52,6 +75,18 @@ def translate(row):
             "Translated content"
         ] = "<p>Pays d'origine: {{ product.metafields.my_fields.country_of_origin.value }}</p><p></p><p>Numéro d'identification du produit 1: {{ product.metafields.my_fields.product_id_1.value }}</p><p></p><p>Numéro d'identification du produit 2: {{ product.metafields.my_fields.product_id_2.value }}</p><p></p><p>(Can dynamically render DIN/NPN if present on product. Missing for most)</p>"
     # Translate Metafields
+    elif row["Type"] == "METAFIELD" and type(row["Default content"]) == str:
+        # Description
+        features = translated_values.loc[
+            translated_values["Key Product Features"]
+            == row["Default content"].replace("\r", "").replace("\n", "")
+        ]
+
+        if len(features) > 0:
+            row["Translated content"] = str(
+                features.iloc[0]["Key Product Features (FR)"]
+            ).replace("_x000D_", "")
+
     return row
 
 
@@ -60,6 +95,18 @@ def translate(row):
 #         print(column)
 
 items = items.apply(translate, axis=1)
+
+print(
+    items.loc[items["Identification"] == "'20169758310448"]["Translated content"]
+    .values[0]
+    .replace("_x000D_", "")
+)
+
+print(
+    items.loc[items["Identification"] == "'20169758310448"]["Translated content"]
+    .values[0]
+    .replace("_x000D_", "")
+)
 
 items.to_excel("formatted_translations.xlsx", index=False)
 items.to_csv("formatted_translations.csv", index=False)
--- scripts/xls_to_csv_shopify.py
+++ scripts/xls_to_csv_shopify.py
@@ -1,7 +1,43 @@
 import pandas as pd
 
 products = pd.read_excel(
-    r'/home/jacobg/Documents/Code/ShopifyScripts/Website document .xlsx')
+    r"/home/jacobg/Documents/Code/ShopifyScripts/spreadsheets/Website document .xlsx"
+)
+
+
+def squish_descriptions(row):
+    description = ""
+
+    cur = 1
+
+    while cur < 6:
+        col = "Key Product Features " + str(cur)
+        if type(row[col]) is str:
+            if len(row[col]) > 0:
+                description += str(row[col]) + "\r\n"
+        cur += 1
+
+    row["Key Product Features"] = description
+
+    descriptionFR = ""
+
+    cur = 1
+
+    while cur < 6:
+        col = "Key Product Features " + str(cur) + " (FR)"
+        if type(row[col]) is str:
+            if len(row[col]) > 0:
+                descriptionFR += str(row[col]) + "\r\n"
+        cur += 1
+
+    row["Key Product Features (FR)"] = descriptionFR
+
+    return row
+
+
+products.apply(squish_descriptions, axis=1).to_excel(
+    "WebsiteDocument2.xlsx", index=False
+)
 
 
 def compile_badges(columns, row):
@@ -21,7 +57,7 @@ def compile_descriptions(columns, row):
         if type(row[column]) is str:
             if len(row[column]) > 0:
                 descriptions = descriptions + str(row[column]) + "\r\n"
-                #descriptions = descriptions + row[column] + ","
+                # descriptions = descriptions + row[column] + ","
 
     return descriptions
 
@@ -40,8 +76,7 @@ def compile_allergens(columns, row):
             contains = contains + column.replace("Contains", "") + " (C)\r\n"
             include_contains = True
         elif row[column] == "M":
-            may_contain = may_contain + \
-                column.replace("Contains", "") + " (M)\r\n"
+            may_contain = may_contain + column.replace("Contains", "") + " (M)\r\n"
             include_may = True
 
     if include_contains:
@@ -58,9 +93,14 @@ def format(row):
     row["Command"] = "MERGE"
     row["Title"] = row["Product Name"]
     row["Body HTML"] = row["Description"]
-    row['Vendor'] = row['Brand']
-    row["Tags"] = row["Category Type"].replace(
-        ",", "") + ", " + row["Category SubType"].replace(",", "") + ',' + row['Brand']
+    row["Vendor"] = row["Brand"]
+    row["Tags"] = (
+        row["Category Type"].replace(",", "")
+        + ", "
+        + row["Category SubType"].replace(",", "")
+        + ","
+        + row["Brand"]
+    )
     row["Tags Command"] = "REPLACE"
     row["Status"] = "Active"
     row["Published"] = "TRUE"
@@ -85,74 +125,122 @@ def format(row):
     row["Variant Inventory Qty"] = 15
     row["Metafield: my_fields.brand_name [single_line_text_field]"] = row["Brand"]
 
-    #size_str = str(row["Product Size"]) + str(row["UOM"])
+    # size_str = str(row["Product Size"]) + str(row["UOM"])
 
     row["Metafield: my_fields.size [single_line_text_field]"] = str(
-        row["Product Size"]) + str(row["UOM"])
+        row["Product Size"]
+    ) + str(row["UOM"])
 
     # badges = compile_badges(["Canadian", "Organic", "GMO Free", "Vegetarian", "Vegan",
     #                         "Fair Trade", "Kosher", "Halal", "Gluten Free", "Bcorp Certified"], row)
 
-    row["Metafield: my_fields.badges [multi_line_text_field]"] = compile_badges(["Canadian", "Organic", "GMO Free", "Vegetarian", "Vegan",
-                                                                                 "Fair Trade", "Kosher", "Halal", "Gluten Free", "Bcorp Certified"], row)
-    row["Metafield: my_fields.description2 [multi_line_text_field]"] = compile_descriptions(
-        ["Key Product Features 1", "Key Product Features 2", "Key Product Features 3", "Key Product Features 4", "Key Product Features 5"], row)
-    row["Metafield: my_fields.dosage [multi_line_text_field]"] = row["Recommended Dosage"]
+    row["Metafield: my_fields.badges [multi_line_text_field]"] = compile_badges(
+        [
+            "Canadian",
+            "Organic",
+            "GMO Free",
+            "Vegetarian",
+            "Vegan",
+            "Fair Trade",
+            "Kosher",
+            "Halal",
+            "Gluten Free",
+            "Bcorp Certified",
+        ],
+        row,
+    )
+    row[
+        "Metafield: my_fields.description2 [multi_line_text_field]"
+    ] = compile_descriptions(
+        [
+            "Key Product Features 1",
+            "Key Product Features 2",
+            "Key Product Features 3",
+            "Key Product Features 4",
+            "Key Product Features 5",
+        ],
+        row,
+    )
+    row["Metafield: my_fields.dosage [multi_line_text_field]"] = row[
+        "Recommended Dosage"
+    ]
     row["Metafield: my_fields.ingredients [multi_line_text_field]"] = row["Ingredients"]
     row["Metafield: my_fields.product_id_1 [single_line_text_field]"] = row["Unit UPC"]
     row["Metafield: my_fields.product_id_2 [single_line_text_field]"] = row["Stock ID"]
-    row["Metafield: my_fields.brand_description [multi_line_text_field]"] = row["Brand Description"]
+    row["Metafield: my_fields.brand_description [multi_line_text_field]"] = row[
+        "Brand Description"
+    ]
     row["Metafield: my_fields.din [single_line_text_field]"] = row["DIN/NPN"]
-    row["Metafield: my_fields.allergens [multi_line_text_field]"] = compile_allergens([
-        "Contains Egg", "Contains Dairy", "Contains Mustard", "Contains Peanuts", "Contains Seafood", "Contains Sesame", "Contains Soy", "Contains Sulfites", "Contains Tree Nuts", "Contains Wheat Gluten"
-    ], row)
-    row["Metafield: my_fields.product_description [multi_line_text_field]"] = row["Description"]
-    row["Metafield: my_fields.country_of_origin [single_line_text_field]"] = row["Country of Origin"]
+    row["Metafield: my_fields.allergens [multi_line_text_field]"] = compile_allergens(
+        [
+            "Contains Egg",
+            "Contains Dairy",
+            "Contains Mustard",
+            "Contains Peanuts",
+            "Contains Seafood",
+            "Contains Sesame",
+            "Contains Soy",
+            "Contains Sulfites",
+            "Contains Tree Nuts",
+            "Contains Wheat Gluten",
+        ],
+        row,
+    )
+    row["Metafield: my_fields.product_description [multi_line_text_field]"] = row[
+        "Description"
+    ]
+    row["Metafield: my_fields.country_of_origin [single_line_text_field]"] = row[
+        "Country of Origin"
+    ]
 
-    row = row.filter(['Stock ID',
-                      'ID',
-                      'Handle',
-                      'Command',
-                      'Title',
-                      'Body HTML',
-                      'Vendor',
-                      'Tags',
-                      'Tags Command',
-                      'Status',
-                      'Published',
-                      'Published Scope',
-                      'Gift Card',
-                      'Row #',
-                      'Top Row',
-                      'Image Src',
-                      'Image Command',
-                      'Image Position',
-                      'Image Alt Text',
-                      'Variant ID',
-                      'Variant Command',
-                      'Variant Position',
-                      'Variant SKU',
-                      'Variant Price',
-                      'Variant Requires Shipping',
-                      'Variant Taxable',
-                      'Variant Image',
-                      'Variant Inventory Tracker',
-                      'Variant Inventory Policy',
-                      'Variant Fulfillment Service',
-                      'Variant Inventory Qty',
-                      'Metafield: my_fields.brand_name [single_line_text_field]',
-                      'Metafield: my_fields.size [single_line_text_field]',
-                      'Metafield: my_fields.badges [multi_line_text_field]',
-                      'Metafield: my_fields.description2 [multi_line_text_field]',
-                      'Metafield: my_fields.dosage [multi_line_text_field]',
-                      'Metafield: my_fields.ingredients [multi_line_text_field]',
-                      'Metafield: my_fields.product_id_1 [single_line_text_field]',
-                      'Metafield: my_fields.product_id_2 [single_line_text_field]',
-                      "Metafield: my_fields.brand_description [multi_line_text_field]",
-                      'Metafield: my_fields.din [single_line_text_field]',
-                      'Metafield: my_fields.allergens [multi_line_text_field]',
-                      'Metafield: my_fields.product_description [multi_line_text_field]',
-                      "Metafield: my_fields.country_of_origin [single_line_text_field]"])
+    row = row.filter(
+        [
+            "Stock ID",
+            "ID",
+            "Handle",
+            "Command",
+            "Title",
+            "Body HTML",
+            "Vendor",
+            "Tags",
+            "Tags Command",
+            "Status",
+            "Published",
+            "Published Scope",
+            "Gift Card",
+            "Row #",
+            "Top Row",
+            "Image Src",
+            "Image Command",
+            "Image Position",
+            "Image Alt Text",
+            "Variant ID",
+            "Variant Command",
+            "Variant Position",
+            "Variant SKU",
+            "Variant Price",
+            "Variant Requires Shipping",
+            "Variant Taxable",
+            "Variant Image",
+            "Variant Inventory Tracker",
+            "Variant Inventory Policy",
+            "Variant Fulfillment Service",
+            "Variant Inventory Qty",
+            "Metafield: my_fields.brand_name [single_line_text_field]",
+            "Metafield: my_fields.size [single_line_text_field]",
+            "Metafield: my_fields.badges [multi_line_text_field]",
+            "Metafield: my_fields.description2 [multi_line_text_field]",
+            "Metafield: my_fields.dosage [multi_line_text_field]",
+            "Metafield: my_fields.ingredients [multi_line_text_field]",
+            "Metafield: my_fields.product_id_1 [single_line_text_field]",
+            "Metafield: my_fields.product_id_2 [single_line_text_field]",
+            "Metafield: my_fields.brand_description [multi_line_text_field]",
+            "Metafield: my_fields.din [single_line_text_field]",
+            "Metafield: my_fields.allergens [multi_line_text_field]",
+            "Metafield: my_fields.product_description [multi_line_text_field]",
+            "Metafield: my_fields.country_of_origin [single_line_text_field]",
+        ]
+    )
 
     return row
 
@@ -161,9 +249,13 @@ formatted_products = products.apply(format, axis=1)
 
 print(products["Brand"].unique())
 
-formatted_products.set_index('Stock ID', inplace=True)
+formatted_products.set_index("Stock ID", inplace=True)
 
 formatted_products.to_excel(
-    "formatted_products.xlsx", sheet_name="Products", index=False)
+    "formatted_products.xlsx", sheet_name="Products", index=False
+)
+
+for col in products:
+    print(col)
 
 print("Complete")
